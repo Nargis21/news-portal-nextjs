c4dab92a4259c07d9c8f37ca1bb113277d3bbcd9
nargis21 <nargisakther1933@gmail.com>
2023-09-26 19:09:16 +0600

    database connected and get data from database done

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,11 @@ import dynamic from 'next/dynamic'
 const HomePage = ({ allNews }) => {
   // console.log(allNews);
 
+  //redux data fetching
   const { data, error, isLoading } = useGetNewsesQuery()
-  console.log(data);
+  // console.log(data);
 
+  //use loading state to component by nextjs dynamic and make it client side rendering
   const DynamicBanner = dynamic(() => import('@/components/UI/Banner'), {
     loading: () => <h1>Loading...</h1>,
     ssr: false
@@ -29,26 +31,30 @@ const HomePage = ({ allNews }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <DynamicBanner />
-      <AllNews allNews={data}></AllNews>
+      <AllNews allNews={allNews}></AllNews>
     </>
   );
 };
 export default HomePage;
 
+// using layout
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
+//data fetching with getServerSideProps ---SSR
 export const getServerSideProps = async () => {
-  const res = await fetch('http://localhost:5000/news')
+  const res = await fetch('http://localhost:3000/api/news')
   const data = await res.json()
+  console.log(data);
   return {
     props: {
-      allNews: data
+      allNews: data.data
     }
   }
 }
 
+//data fetching with getStaticProps ----SSG
 // export const getStaticProps = async () => {
 //   const res = await fetch('http://localhost:5000/news')
 //   const data = await res.json()
